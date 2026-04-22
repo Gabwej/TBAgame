64accbd4fcb1d727e13720345b72be28e9b0df3b
l <l.l@gmail.com>
2026-04-22 21:50:18 +0200

    Second commit * created base for the pygame display - need to create fundamental files that store parts of the game

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-print("hello world")
 
+from sys import exit
+import pygame
+import intro
 
-def hello_world():
-    print("hello world")
+pygame.init()
+
+# the display for the game
+size = (1000, 600)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('A Certain Text Based Adventure')
+clock = pygame.time.Clock()
+
+test_surface = pygame.surface.Surface((150, 200))
+test_surface.fill((255,255,255))
+
+while True:
+    # searches for inputs from player
+    for event in pygame.event.get():
+        # quits the game if you press x on the window
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    # draws elements
+
+    screen.blit(test_surface, (200,100))
+
+    # updates the game
+    pygame.display.update()
+    clock.tick(60)
